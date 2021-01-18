d0b3e046156e64929e8e8a932f48dc3def68f78a
Barkley Santo <barkley.santo@gmail.com>
2021-01-18 14:05:58 -0600

    added style constant to see outline between different folders

--- App.js
+++ App.js
@@ -1,18 +1,28 @@
 import React from 'react';
 
 function App() {
-  return <div>
-    <Folder name="Desktop" />
-    <Folder name="Applications" />
-    <File name="dogs.jpeg" />
-  </div>
+  return (
+    <div>
+      <Folder name="Desktop">
+        <File name="dogs.jpeg" />
+        <File name="cats.jpeg" />
+      </Folder>
+      <Folder name="Applications" />
+
+    </div>
+  )
 }
 
 const Folder = (props) => {
-  return <h4>{props.name}</h4>
+  console.log(props)
+  const borderStyle = { border: '2px solid blue' };
+  return <div style={borderStyle}>
+    {props.name}
+    {props.children}
+  </div>
 }
 
 const File = (props) => {
-  return <h5>{props.name}</h5>
+  return <div>{props.name}</div>
 }
 export default App;
