3cbb373d4965af5ef89fccf72d0960ef38d12e3e
Barkley Santo <barkley.santo@gmail.com>
2021-01-18 14:47:55 -0600

    conditionally render icon depending on file type for each file

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ function App() {
           <File name="express_file.mp4" />
         </Folder>
         <File name="dogs.jpeg" />
-        <File name="cats.jpeg" />
+        <File name="cats.png" />
       </Folder>
       <Folder name="Applications" />
 
@@ -21,10 +21,14 @@ const Folder = (props) => {
   // const borderStyle = { border: '2px solid blue' };
   const [isOpen, setIsOpen] = useState(true);
   const { name, children } = props;
+  const direction = isOpen ? 'down' : 'right';
   const handleClick = () => setIsOpen(!isOpen);
 
   return <div>
-    <span onClick={handleClick}><i className="folder icon"></i></span>
+    <span onClick={handleClick}>
+      <i className="blue folder icon"></i>
+      <i className={`caret ${direction} icon`}></i>
+    </span>
     {name}
     <div style={{ marginLeft: '15px' }}>
       {isOpen ? children : null}
@@ -33,6 +37,16 @@ const Folder = (props) => {
 }
 
 const File = (props) => {
-  return <div>{props.name}</div>
+  const { name } = props;
+  const fileExtension = name.split('.')[1];
+  const fileIcons = {
+    mp4: 'headphones',
+    jpeg: 'file image',
+    png: 'file image outline'
+  }
+  return <div>
+    <i className={`${fileIcons[fileExtension]} icon `}></i>
+    {props.name}
+  </div>
 }
 export default App;
