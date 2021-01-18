aa975c474b7c5668d2abc6c0e0387ea3fd3ccbba
Barkley Santo <barkley.santo@gmail.com>
2021-01-18 13:56:29 -0600

    Folder component and File component created

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 function App() {
-  return 'hello'
+  return <div>
+    <Folder name="Desktop" />
+    <Folder name="Applications" />
+    <File name="dogs.jpeg" />
+  </div>
 }
 
+const Folder = (props) => {
+  return <h4>{props.name}</h4>
+}
+
+const File = (props) => {
+  return <h5>{props.name}</h5>
+}
 export default App;
